e68b84b190565daf120434a8da5755fc8ed74ae1
Roli <rroycehg@gmail.com>
2021-06-29 18:20:02 -0400

    Finished

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+  const projObjects = projects.map((project) => <ProjectItem key={project.id} {...project} /> )
+  
   console.log(projects);
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projObjects}
+        </div>
     </div>
   );
 }
